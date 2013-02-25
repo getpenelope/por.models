6b795d2ea873ff25da1a81ce51ce31f5e011aa7e
Andrew Mleczko <andrew@mleczko.net>
2013-02-25 16:07:18 +0100

    Preparing release 1.3.13

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if sys.version_info[:3] < (2, 5, 0):
     requires.append('pysqlite')
 
 setup(name='por.models',
-      version='1.3.13.dev0',
+      version='1.3.13',
       description='SQLAlchemy models for Penelope',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
